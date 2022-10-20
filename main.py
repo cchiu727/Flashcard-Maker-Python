5818b812c0762cfec2c726e622a9766c951d0f43
Collin Chiu <collinchiu23@gmail.com>
2022-10-20 12:12:20 -0700

    Add getChoice function, edit menu function

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 # main.py
+def getChoice():
+    while True:
+        try:
+            choice = int(input("Enter a choice: "))
+        except ValueError:
+            print("Invalid choice.")
+            continue
+        if choice not in (1, 2, 3, 4):
+            print("Invalid choice.")
+            continue
+        else:
+            break
+    return choice
 
 def menu():
     choice = 0
@@ -7,8 +20,8 @@ def menu():
         (2) Review a flashcard set
         (3) Edit a flashcard set
         (4) Delete a flashcard set""")
-    choice = input("Enter a choice: ")
-    print(choice)
+    
+    choice = getChoice()
 
 
 print("Welcome to Flashcard Maker!")
